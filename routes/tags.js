4333419f25e301540021d5a94aba4161f8d3f218
Maxence <raingeonneau.m@gmail.com>
2021-06-07 15:26:55 +0200

    .

--- routes/tags.js
+++ routes/tags.js
@@ -8,26 +8,4 @@ router.get('/', (req, res) => {
   res.json(fakeTags);
 });
 
-router.get('/:tagId', (req, res) => {
-  const tagId = Number(req.params.tagId);
-  const foundTag = fakeTags.find((tag) => tag.id === tagId);
-  if (!foundTag) {
-    return res.status(404).json({
-      error: 'Tag not found',
-    });
-  }
-  return res.json(foundTag);
-});
-
-router.get('/:tagId/posts', (req, res) => {
-  const tagId = Number(req.params.tagId);
-  const foundTag = allPosts.filter((post) => post.tag_ids.includes(tagId));
-  if (!foundTag) {
-    return res.status(404).json({
-      error: 'Post not found',
-    });
-  }
-  return res.json(foundTag);
-});
-
 module.exports = router;
