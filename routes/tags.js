850fcf463187b13b969a0770e2bcdb99bbe67f8c
Maxence <raingeonneau.m@gmail.com>
2021-06-07 14:26:46 +0200

    corrigé

--- routes/tags.js
+++ routes/tags.js
@@ -3,9 +3,27 @@ const fakeTags = require('../data/tags');
 
 const router = express.Router();
 
-// Get a list of comments
-router.get('/', (req, res) => {
-  res.json(fakeTags);
+// Get a single post
+router.get('/:tagId', (req, res) => {
+  const tagId = Number(req.params.tagId);
+  const foundTag = fakeTags.find((tag) => tag.id === tagId);
+  if (!foundTag) {
+    return res.status(404).json({
+      error: 'Tag not found',
+    });
+  }
+  return res.json(foundTag);
+});
+
+router.get('/:tagId/posts', (req, res) => {
+  const tagId = Number(req.params.tagId);
+  const foundTag = allPosts.filter((post) => post.tag_ids.includes(tagId));
+  if (!foundTag) {
+    return res.status(404).json({
+      error: 'Post not found',
+    });
+  }
+  return res.json(foundTag);
 });
 
 module.exports = router;
